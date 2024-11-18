a40ce2f6368eb8addb196fc9c01fe0d12dfe58bd
jeongsungjin <okjsj0222@kookmin.ac.kr>
2024-11-19 00:55:09 +0900

    1119 테스트 시작

--- src/obstacle_detector/src/tunnel_drive_pivot.py
+++ src/obstacle_detector/src/tunnel_drive_pivot.py
@@ -4,34 +4,33 @@
 import rospy
 from obstacle_detector.msg import Obstacles
 from std_msgs.msg import String, Int32, Bool
-from sensor_msgs.msg import PointCloud  
+from sensor_msgs.msg import PointCloud
 from lane_detection.msg import Drive_command
-import math
 
 class TUNNEL:
     def __init__(self):
         # Subscribers
-        rospy.Subscriber("/mode", String, self.modeCB)  # /mode를 modeCB로 연결
-        rospy.Subscriber("/roi_points_tunnel", PointCloud, self.roiPointsCB)  # /roi_points_tunnel을 roiPointsCB로 연결
+        rospy.Subscriber("/mode", String, self.modeCB)
+        rospy.Subscriber("/roi_points_tunnel", PointCloud, self.roiPointsCB)
         rospy.Subscriber("/yellow_pixel", Int32, self.yellowCB)
         rospy.Subscriber("/crossing_gate_done", Int32, self.crossing_gate_done_callback)
 
-        # Publishers
+        # Publisher
         self.ctrl_cmd_pub = rospy.Publisher('/motor_tunnel', Drive_command, queue_size=1)
-
-        # 메시지 객체 초기화
-        self.ctrl_cmd_msg = Drive_command()
+        self.tunnel_done_pub = rospy.Publisher('/tunnel_done', Bool, queue_size=1)
 
         # 변수 초기화
-        self.roi_points = []  # ROI 포인트 리스트
+        self.roi_points = []
         self.x_points = []
         self.y_points = []
-        self.mode = ''  # 현재 모드
-        self.flag = False
-        self.yellow_pixels = 0  # 노란색 픽셀 개수
-        self.speed = 0.3  # 기본 속도
-        self.steer = 0.0  # 조향각
-        self.crossing_completed = True #원래 트루임 테스트 하기위해 잠시 올린것
+        self.mode = ''
+        self.yellow_pixels = 0
+        self.crossing_completed = False
+        self.tunnel_done_flag = False
+        self.speed = 0.3
+        self.steer = 0.0
+        self.max_steer = 100.0  # 조향각 제한
+        self.steer_decay = 0.9  # 감쇠 비율
         self.rate = rospy.Rate(30)
 
         # 메인 루프
@@ -40,48 +39,61 @@ class TUNNEL:
             self.rate.sleep()
 
     def control_loop(self):
+        # 특정 모드에서는 조향 제어를 하지 않음
         if self.mode in ['RABACON', 'SIGN', 'DYNAMIC', 'STATIC']:
             return
 
-        # 노란색 픽셀 수와 ROI 포인트 기반 제어
-        if self.yellow_pixels < 8000 and self.crossing_completed == True:
-            if self.y_points and self.y_points[0] < 0 :  # ROI 첫 포인트의 y값이 0보다 작을 경우
-                self.steer += 3
-                self.publishCtrlCmd(self.speed, self.steer, True)
+        # 노란색 픽셀 및 게이트 상태 확인
+        if self.yellow_pixels < 8000 and self.crossing_completed:
+            # ROI 첫 번째 포인트 기준으로 조향 결정
+            if self.y_points:
+                if self.y_points[0] < 0:  # 왼쪽 벽에 가까운 경우
+                    self.steer += 50
+                elif self.y_points[0] > 0:  # 오른쪽 벽에 가까운 경우
+                    self.steer -= 50
+                else:  # 중앙에 가까운 경우
+                    self.steer = 0.0  # 감쇠 적용
+            if self.yellow_pixels > 10000:
+                self.tunnel_done_flag
+                self.publish_tunnel_done(self.tunnel_done_flag)
+            else:
+                self.steer = 0.0  # ROI 데이터가 없을 경우 감쇠 적용
 
-            elif self.y_points and self.y_points[0] > 0:  # ROI 첫 포인트의 y값이 0보다 클 경우
-                self.steer -= 3
-                self.publishCtrlCmd(self.speed, self.steer, True)
+            # 조향값 제한
+            self.steer = max(min(self.steer, self.max_steer), -self.max_steer)
 
-            else:  
-                self.steer = 0.0
-                self.publishCtrlCmd(self.speed, self.steer, True)
+            # 제어 명령 퍼블리시
+            self.publishCtrlCmd(self.speed, self.steer, True)
         else:
-            self.publishCtrlCmd(self.speed, self.steer, False)
-
+            # 조건을 충족하지 못할 경우 제어 비활성화
+            self.publishCtrlCmd(self.speed, 0.0, False)
 
     def crossing_gate_done_callback(self, msg):
         self.crossing_completed = msg.data
 
+    def publish_tunnel_done(self, tunnel_done):
+        self.tunnel_done_pub.publish(tunnel_done)
+
     def roiPointsCB(self, msg):
-        self.roi_points = []  
-        self.x_points = []  
-        self.y_points = []  
+        self.roi_points = []
+        self.x_points = []
+        self.y_points = []
 
         for point in msg.points:
-            self.roi_points.append((point.x, point.y))  
-            self.x_points.append(point.x)  
-            self.y_points.append(point.y)  
+            self.roi_points.append((point.x, point.y))
+            self.x_points.append(point.x)
+            self.y_points.append(point.y)
 
     def yellowCB(self, msg):
         self.yellow_pixels = msg.data
 
     def modeCB(self, msg):
-        self.mode = msg.data  # /mode 토픽 데이터 저장
+        self.mode = msg.data
 
     def publishCtrlCmd(self, motor_msg, servo_msg, flag):
-        self.ctrl_cmd_msg.speed = (motor_msg)
-        self.ctrl_cmd_msg.angle = (servo_msg)
+        self.ctrl_cmd_msg = Drive_command()
+        self.ctrl_cmd_msg.speed = motor_msg
+        self.ctrl_cmd_msg.angle = servo_msg
         self.ctrl_cmd_msg.flag = flag
         self.ctrl_cmd_pub.publish(self.ctrl_cmd_msg)
 
